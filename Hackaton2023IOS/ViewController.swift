e38a49f7ef37c9c37b3a008c3a0930b2f5254ff6
Andrii Lytvyniuk <litvinyukwork@gmail.com>
2023-05-12 18:22:13 +0300

    Initial Commit

---
+++ Hackaton2023IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackaton2023IOS
+//
+//  Created by Andrii Lytvyniuk on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
