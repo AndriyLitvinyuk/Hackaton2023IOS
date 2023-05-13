2cc3660739a09eacacf9a9c2e75abe7dce657cce
Andrii Lytvyniuk <litvinyukwork@gmail.com>
2023-05-13 17:22:49 +0300

    clean up

--- Hackaton2023IOS/Presentation/Screens/Profile/ProfileViewController.swift
+++ Hackaton2023IOS/Presentation/Screens/Profile/ProfileViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 final class ProfileViewController: UIViewController, StoryboardInstantiable {
-    let tableViewDataSource = ProfileTableDataSource()
     var viewModel: ProfileViewModel? {
         didSet {
             viewDidLoad()
